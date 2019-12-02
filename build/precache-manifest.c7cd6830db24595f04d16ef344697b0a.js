c8d8e97fe8795a9b8c4d967f5f2d22c5dcdcdcb8
blitzly <adriangerig@gmail.com>
2019-12-02 16:08:07 -0500

    Adding react build

---
+++ build/precache-manifest.c7cd6830db24595f04d16ef344697b0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "051c29c6d80d90d63d552b1430b045dc",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "5bc3340873bb34db3b71",
+    "url": "/react-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b7ddff04b40ed44bfe3f",
+    "url": "/react-app/static/js/2.8d76799d.chunk.js"
+  },
+  {
+    "revision": "5bc3340873bb34db3b71",
+    "url": "/react-app/static/js/main.f99b6d0a.chunk.js"
+  },
+  {
+    "revision": "bda65952d872243e3943",
+    "url": "/react-app/static/js/runtime-main.3cccc921.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-app/static/media/logo.25bf045c.svg"
+  }
+]);
